485247944e7b941ae180501f94afe2afd5a8c678
Konstantin Volkov <wallcaw@wallcaw.ru>
2021-05-16 15:28:47 +0300

    Добавлена анимация формы поиска, сворачивание/разворачивание боковой панели, переделана ссылка профиля в шапке

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function(e) {    
-    let circle = document.querySelectorAll('.progress-ring__circle');
-    let text = document.querySelectorAll('.progress-ring__text');
+document.addEventListener("DOMContentLoaded", function(e) {    
+    let circle = document.querySelectorAll(".progress-ring__circle");
+    let text = document.querySelectorAll(".progress-ring__text");
     let radius = 0;
     let circumference = 0;
     let offset = 0;
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
         circumference = radius * Math.PI * 2;
         offset = circumference - percent / 100 * circumference;
 
-        circle[index].style.strokeDasharray = circumference + ' ' + circumference;
+        circle[index].style.strokeDasharray = circumference + " " + circumference;
         circle[index].style.strokeDashoffset = offset;
         text[index].textContent = percent;
     }    
@@ -18,11 +18,29 @@ document.addEventListener('DOMContentLoaded', function(e) {
     for (let i = 0; i < text.length; i++) {
         setProgress(text[i].textContent, i);
     }
-}, false);
 
-document.addEventListener("DOMContentLoaded", function() {
+    let sidebarPanel = document.querySelector("#sidebar-panel");
+    let openPanelButton = document.querySelectorAll(".js-sidebar-toggle");
     let closePanelButton = document.querySelector("#panel-close");
+
+    for (let j = 0; j < openPanelButton.length; j++) {
+        openPanelButton[j].addEventListener("click", function(e) {
+            e.preventDefault();
+            // this.parentNode.querySelector(".header__profile__menu__icon").classList.toggle("icon-arrow-up");
+            sidebarPanel.classList.toggle("active");
+        }, false);
+    }
+
     closePanelButton.addEventListener("click", function() {
-        document.querySelector("#sidebar-panel").classList.remove("active");
+        sidebarPanel.classList.remove("active");
     }, false);
+
+    let searchInput = document.querySelector("#search");
+    
+    ["focus", "blur"].forEach(function(e) {
+        searchInput.addEventListener(e, function() {
+            this.parentNode.classList.toggle("active");
+        }, false);
+    });
+
 }, false);
